86f22225d9290136f1d69e128a6cc546f9fa6fbf
mjdude <mohammed.jalil@gmail.com>
2018-02-23 22:26:41 +0800

    updated test to fix breaking changes with web 1.0.0 beta26

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,21 +1,32 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
 
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
-beforeEach(() => {
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+beforeEach( async () => {
     // Get a list of all accounts
-    web3.eth.getAccounts()
-        .then( fetchedAccounts => {
-            console.log(fetchedAccounts);
-        })
-
+    accounts= await web3.eth.getAccounts();
 
     // Use one of the accounts to deploy the contracts
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there']})
+        .send({ from: accounts[0], gas: '1000000' });
+    
+        inbox.setProvider(provider);
 })
 
 describe("Inbox",() => {
     it("deploys a contract", () => {
+        assert.ok(inbox.options.address);
     });
+
+    it('Sets default message', () => {
+
+    })
 })
